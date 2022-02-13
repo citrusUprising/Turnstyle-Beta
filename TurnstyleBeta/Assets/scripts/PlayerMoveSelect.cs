f6bfd4727e2a3a8c25b8ebd6d12e36cbf75c2a34
Sky Peterson <82342913+skyptrson@users.noreply.github.com>
2022-02-12 18:20:24 -0800

    Created player objects & display health.

--- TurnstyleBeta/Assets/scripts/PlayerMoveSelect.cs
+++ TurnstyleBeta/Assets/scripts/PlayerMoveSelect.cs
@@ -7,11 +7,20 @@ using TMPro;
 public class PlayerMoveSelect : MonoBehaviour
 {
     // public int character;
-    public Sprite Blue;
-    public Sprite Yellow;
-    public Sprite Pink;
-    public Sprite Green;
-    public Sprite Red;
+    // 🧍‍♀️ character objects
+    // public GameObject Blue;
+    // public GameObject Yellow;
+    // public GameObject Pink;
+    // public GameObject Green;
+    // public GameObject Red;
+    // public GameObject moveSelect;
+
+    //  🖼 player menu images
+    // public Sprite BlueImg;
+    // public Sprite YellowImg;
+    // public Sprite PinkImg;
+    // public Sprite GreenImg;
+    // public Sprite RedImg;
     private Image menuImg;
 
     // 📛 text
@@ -21,7 +30,6 @@ public class PlayerMoveSelect : MonoBehaviour
     private GameObject move1;
     private GameObject move2;
     private GameObject move3;
-    private bool isYellow;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,55 +43,54 @@ public class PlayerMoveSelect : MonoBehaviour
     }
     
     public void ChangeColor(int character){
-        menuImg = this.GetComponent<Image>();
-        NameTxt = transform.GetChild(0).gameObject;
-        move1 = transform.GetChild(2).gameObject;
-        move2 = transform.GetChild(3).gameObject;
-        move3 = transform.GetChild(4).gameObject;
-        if(character == 0){
-            // beverly
-            menuImg.sprite = Blue;
-            NameTxt.GetComponent<TextMeshProUGUI>().text = "Beverly";
-            move1.GetComponent<TextMeshProUGUI>().text = "#Girl";
-            move2.GetComponent<TextMeshProUGUI>().text = ":)";
-            move3.GetComponent<TextMeshProUGUI>().text = "<3";
-            
-        }
-        if(character == 1){
-            // amery
-            menuImg.sprite = Yellow;
-            NameTxt.GetComponent<TextMeshProUGUI>().text ="Amery";
-            move1.GetComponent<TextMeshProUGUI>().text = ":(";
-            move2.GetComponent<TextMeshProUGUI>().text = "move 2";
-            move3.GetComponent<TextMeshProUGUI>().text = "peggle 2!";
-        }
-        if(character == 2){
-            // koralie
-            menuImg.sprite = Pink;
-            NameTxt.GetComponent<TextMeshProUGUI>().text = "Koralie";
-            move1.GetComponent<TextMeshProUGUI>().text = "";
-            move2.GetComponent<TextMeshProUGUI>().text = "";
-            move3.GetComponent<TextMeshProUGUI>().text = "";
-        }
-        if(character == 3){
-            // jade
-            menuImg.sprite = Green;
-            NameTxt.GetComponent<TextMeshProUGUI>().text = "Jade";
-            move1.GetComponent<TextMeshProUGUI>().text = "";
-            move2.GetComponent<TextMeshProUGUI>().text = "";
-            move3.GetComponent<TextMeshProUGUI>().text = "";
-        }
-        if(character == 4){
-            // seraphim
-            menuImg.sprite = Red;
-            NameTxt.GetComponent<TextMeshProUGUI>().text = "Seraphim";
-            move1.GetComponent<TextMeshProUGUI>().text = "";
-            move2.GetComponent<TextMeshProUGUI>().text = "";
-            move3.GetComponent<TextMeshProUGUI>().text = "";
-        }
+        // menuImg = this.GetComponent<Image>();
+        // NameTxt = transform.GetChild(0).gameObject;
+        // move1 = transform.GetChild(2).gameObject;
+        // move2 = transform.GetChild(3).gameObject;
+        // move3 = transform.GetChild(4).gameObject;
+        // if(character == 0){
+        //     // beverly
+        //     menuImg.sprite = BlueImg;
+        //     NameTxt.GetComponent<TextMeshProUGUI>().text = Blue.GetComponent<Friendly>().name;
+        //     move1.GetComponent<TextMeshProUGUI>().text = "#Girl";
+        //     move2.GetComponent<TextMeshProUGUI>().text = ":)";
+        //     move3.GetComponent<TextMeshProUGUI>().text = "<3";            
+        // }
+        // if(character == 1){
+        //     // amery
+        //     menuImg.sprite = YellowImg;
+        //     NameTxt.GetComponent<TextMeshProUGUI>().text = Yellow.GetComponent<Friendly>().name;
+        //     move1.GetComponent<TextMeshProUGUI>().text = ":(";
+        //     move2.GetComponent<TextMeshProUGUI>().text = "move 2";
+        //     move3.GetComponent<TextMeshProUGUI>().text = "peggle 2!";
+        // }
+        // if(character == 2){
+        //     // koralie
+        //     menuImg.sprite = PinkImg;
+        //     NameTxt.GetComponent<TextMeshProUGUI>().text = Pink.GetComponent<Friendly>().name;
+        //     move1.GetComponent<TextMeshProUGUI>().text = "";
+        //     move2.GetComponent<TextMeshProUGUI>().text = "";
+        //     move3.GetComponent<TextMeshProUGUI>().text = "";
+        // }
+        // if(character == 3){
+        //     // jade
+        //     menuImg.sprite = GreenImg;
+        //     NameTxt.GetComponent<TextMeshProUGUI>().text = Green.GetComponent<Friendly>().name;
+        //     move1.GetComponent<TextMeshProUGUI>().text = "";
+        //     move2.GetComponent<TextMeshProUGUI>().text = "";
+        //     move3.GetComponent<TextMeshProUGUI>().text = "";
+        // }
+        // if(character == 4){
+        //     // seraphim
+        //     menuImg.sprite = RedImg;
+        //     NameTxt.GetComponent<TextMeshProUGUI>().text = Red.GetComponent<Friendly>().name;
+        //     move1.GetComponent<TextMeshProUGUI>().text = "";
+        //     move2.GetComponent<TextMeshProUGUI>().text = "";
+        //     move3.GetComponent<TextMeshProUGUI>().text = "";
+        // }
         //if [current player] is [x]
             // change color
             // change to correct ability list w/ descriptors
-        
+        // NameTxt.GetComponent<TextMeshProUGUI>().text = nameTag        
     }
 }
