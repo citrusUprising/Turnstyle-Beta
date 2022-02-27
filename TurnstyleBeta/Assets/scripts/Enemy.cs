06cec69c265ea366a0f2e13ae0b27881c470f616
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2022-02-26 17:05:08 -0800

    added color change

--- TurnstyleBeta/Assets/scripts/Enemy.cs
+++ TurnstyleBeta/Assets/scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : Unit
 {
@@ -20,8 +21,8 @@ public class Enemy : Unit
         Status buff = new Status(StatusType.Buff, StatusName.None, 0, 0);
         Status debuff = new Status(StatusType.Debuff, StatusName.None, 0, 0);
         this.statuses = new Status[] { health, buff, debuff };
-        var rend = this.GetComponent<Renderer> ();
-        Color a = rend.material.color;
+        var rend = this.GetComponent<Image>();
+        Color a;
         
         switch (this.unitName){
 
@@ -29,42 +30,42 @@ public class Enemy : Unit
             this.hp = 15;
             this.maxHP = 15;
             this.abilities = new Ability[] { new BasicAttack(), new Temp14(), new Crush() };
-            a = new Color(0,0,0,1);
+            a = new Color(0.95f,1f,0.5f,1);
             break;
 
             case "Fragile": case "Fragile A": case "Fragile B": case "Fragile C":
             this.hp = 7;
             this.maxHP = 7;
             this.abilities = new Ability[] { new Temp15(), new Temp16(), new Temp17() };
-            a = new Color(0,0,0,1);
+            a = new Color(0.5f,0.8f,1,1);
             break;
 
             case "Beefy": case "Beefy A": case "Beefy B": case "Beefy C":
             this.hp = 20;
             this.maxHP = 20;
             this.abilities = new Ability[] { new Hunker(), new Temp14(), new Slump() };
-            a = new Color(0,0,0,1);
+            a = new Color(1,0.5f,0.4f,1);
             break;
 
             case "Support": case "Support A": case "Support B": case "Support C":
             this.hp = 10;
             this.maxHP = 10;
             this.abilities = new Ability[] { new Temp18(), new Temp19(), new Temp20() };
-            a = new Color(0,0,0,1);
+            a = new Color(1,0.6f,0.8f,1);
             break;
 
             case "Trickster": case "Trickster A": case "Trickster B": case "Trickster C":
             this.hp = 12;
             this.maxHP = 12;
             this.abilities = new Ability[] { new Temp21(), new Temp22(), new Temp10() };
-            a = new Color(0,0,0,1);
+            a = new Color(0.4f,0.9f,0.5f,1);
             break;
 
             case "Yellow": case "Yellow A": case "Yellow B": case "Yellow C":
             this.hp = 15;
             this.maxHP = 15;
             this.abilities = new Ability[] { new Temp01(), new Temp02(), new Repel() };
-            a = new Color(1,1,0,1);
+            a = new Color(0.7f,0.8f,0.1f,1);
             break;
 
             case "Red": case "Red A": case "Red B": case "Red C":
@@ -72,7 +73,7 @@ public class Enemy : Unit
             this.maxHP = 20;
             this.immunity = new StatusName[] {StatusName.Distracted};
             this.abilities = new Ability[] { new Temp06(), new Temp07(), new Temp08() };
-            a = new Color(1,0,0,1);
+            a = new Color(0.5f,0.15f,0.15f,1);
             break;
 
             case "Blue": case "Blue A": case "Blue B": case "Blue C":
@@ -80,7 +81,7 @@ public class Enemy : Unit
             this.maxHP = 25;
             this.immunity = new StatusName[] {StatusName.Flinch};
             this.abilities = new Ability[] { new Temp03(), new Temp04(), new Temp05() };
-            a = new Color(0,0,1,1);
+            a = new Color(0,0.3f,0.4f,1);
             break;
 
             case "Green": case "Green A": case "Green B": case "Green C":
@@ -88,14 +89,14 @@ public class Enemy : Unit
             this.maxHP = 16;
             this.immunity = new StatusName[] {StatusName.StrungOut};
             this.abilities = new Ability[] { new Repel(), new Temp09(), new Temp10() };
-            a = new Color(0,1,0,1);
+            a = new Color(0.15f,0.35f,0.15f,1);
             break;
 
             case "Pink": case "Pink A": case "Pink B": case "Pink C":
             this.hp = 19;
             this.maxHP = 19;
             this.abilities = new Ability[] { new Temp11(), new Temp12(), new Temp13() };
-            a = new Color(1,(float)0.5,(float)0.5);
+            a = new Color(0.7f,0.1f,0.4f);
             break;
 
             default:
@@ -107,7 +108,7 @@ public class Enemy : Unit
             break;
 
         }
-        rend.material.color = a;
+        rend.color=a;
     }
 
     // Update is called once per frame
